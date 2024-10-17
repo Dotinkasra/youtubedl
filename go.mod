ec9a3a8d32bfe3302d7aaf70e80311df9dc77584
Dotinkasra <tyekepon@pm.me>
2024-10-18 05:10:05 +0900

    init

---
+++ go.mod
@@ -0,0 +1,12 @@
+module youtubedl
+
+go 1.23.2
+
+require (
+	github.com/ProtonMail/go-crypto v1.0.0 // indirect
+	github.com/cloudflare/circl v1.5.0 // indirect
+	github.com/lrstanley/go-ytdlp v0.0.0-20241009011144-88433fef3229 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+	"path/filepath"
+	"sync"
+
+	"github.com/lrstanley/go-ytdlp"
+	"gopkg.in/yaml.v3"
+)
+
+//go:embed channels.yaml
+var file []byte
+
+type Channel struct {
+	Name      string `yaml:"name"`
+	URL       string `yaml:"url"`
+	Directory string `yaml:"directory"`
+}
+
+type Targets struct {
+	Channels []Channel
+}
+
+func (t *Targets) getChannels() []Channel {
+	var config []Channel
+	err := yaml.Unmarshal(file, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	return config
+}
+
+func (c *Channel) download(wg *sync.WaitGroup) {
+	dl := ytdlp.New().
+		FormatSort("asr,abr,res,ext:webm:ogg").
+		EmbedThumbnail().
+		EmbedMetadata().
+		CookiesFromBrowser("chrome").
+		Paths(c.Directory).
+		DownloadArchive(filepath.Join(c.Directory, "download")).
+		Output("%(upload_date)s_%(title)s.%(ext)s")
+
+	_, err := dl.Run(context.TODO(), c.URL)
+	if err != nil {
+		panic(err)
+	}
+
+	wg.Done()
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	t := Targets{}
+	channels := t.getChannels()
+	for _, channel := range channels {
+		wg.Add(1)
+		go channel.download(&wg)
+	}
+	wg.Wait()
+	fmt.Println(channels)
+}
